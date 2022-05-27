8fe0b61ac4cd12074bc23def400aa6cff027e135
vishal <vishal.malhotra@prepladder.com>
2022-05-27 14:31:48 +0530

    day2

--- app/src/main/java/com/prepladder/medical/mydaggerhiltapp/MainActivity.kt
+++ app/src/main/java/com/prepladder/medical/mydaggerhiltapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -12,6 +13,9 @@ import androidx.compose.material.Card
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -32,121 +36,56 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Column(Modifier.fillMaxSize()) {
 
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
 
-            val fontFamiliy = FontFamily(
-                Font(R.font.robotomono_bold, FontWeight.ExtraBold)
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color(0xFF101010))
-            ) {
-                Text(
-
-                    text = buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(Color.Green,
-                                fontSize = 30.sp)
-                        ){
-                            append("J")
-                        }
-                        append("etpack")
-                        withStyle(
-                            style = SpanStyle(Color.Green,
-                                fontSize = 30.sp)
-                        ){
-                            append("C")
-                        }
-                        append("ompose")
-                    },
-                    color = Color.White,
-                    fontSize = 30.sp,
-                    fontFamily = fontFamiliy,
-                    fontStyle = FontStyle.Italic,
-                    textAlign = TextAlign.Center,
-                    textDecoration = TextDecoration.Underline
-
+                ColorBox(
+                    Modifier
+                        .weight(0.5f)
+                        .fillMaxSize()
+                ) {
+                    color.value = it
+                }
+                Box(
+                    modifier = Modifier
+                        .background(color = color.value)
+                        .weight(1f)
+                        .fillMaxSize()
                 )
-            }
 
-
-            val painter = painterResource(id = R.drawable.img)
-            val description = " IMAGE? "
-            val title = " CRICKET IMAGE "
-
-            Box(
-                modifier = Modifier
-                    .padding(10.dp)
-                    .fillMaxHeight(0.5f)
-                    .padding(16.dp)
-            ) {
-                ImageCard(
-                    painter = painter,
-                    contentDescription = description,
-                    title = title
-                )
             }
         }
     }
 
+
     @Composable
-    fun ImageCard(
-        painter: Painter,
-        contentDescription: String,
-        title: String,
-        modifier: Modifier = Modifier
+    fun ColorBox(modifier: Modifier, updateColor: (Color) -> Unit) {
 
-    ) {
+        Box(modifier = modifier
+            .background(Color.Red)
 
-        Card(
-            modifier = modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(10.dp),
-            elevation = 5.dp
-        ) {
-            Box(modifier = Modifier.height(200.dp)) {
-                Image(
-                    painter = painter,
-                    contentDescription = contentDescription,
-                    contentScale = ContentScale.Crop
-                )
+            .clickable {
+                updateColor(
+                    Color(
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        1f
 
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(
-                            brush = Brush.verticalGradient(
-                                colors = listOf(
-                                    Color.Transparent,
-                                    Color.Black,
-
-                                    ),
-                                startY = 300f
-                            )
-                        )
-                )
-                Box(
-                    modifier = modifier
-                        .fillMaxHeight()
-                        .padding(12.dp),
-
-                    contentAlignment = Alignment.BottomStart
-                ) {
-
-                    Text(
-                        text = "VISHAL MALHOTRA",
-                        style = TextStyle(color = Color.White),
-                        fontSize = 16.sp
                     )
-                }
-            }
-        }
+                )
 
+            }
+        )
     }
 
 
